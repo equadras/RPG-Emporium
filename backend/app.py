f88792a54f00c473de1a13b9a4f096fc288ff375
thurunany <thuany.muraro@gmail.com>
2025-07-08 16:11:20 -0300

    update

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,16 @@ bcrypt = Bcrypt(app)
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
+    origin = request.headers.get('Origin')
+    allowed_origins = [
+        'http://localhost:5173',
+        f'https://5173-{os.getenv("CODESPACE_NAME", "")}.' + os.getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', 'app.github.dev'),
+        f'https://5000-{os.getenv("CODESPACE_NAME", "")}.' + os.getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', 'app.github.dev'),
+    ]
+    if origin in allowed_origins or origin is None:
+        response.headers['Access-Control-Allow-Origin'] = origin or '*'
+    else:
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
